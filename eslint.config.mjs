15144b85b9137dc28f11366ba64215513f48fe38
samrat-chauhan24 <chauhansamrat835@gmail.com>
2026-08-25 14:57:35 +0530

    fix: made it deployment ready

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from "eslint-config-next/core-web-vitals.js";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
+  nextVitals,
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
